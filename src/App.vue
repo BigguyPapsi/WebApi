ca1019fca837da38226c170bb69ac450e85c654b
BigguyPapsi <papartztv@gmail.com>
2023-04-22 01:39:00 +0700

    hello there

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <p>ເວີຊັ້ນ {{item[0].version}}</p>
+    <router-view> </router-view>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+ data(){
+  return{item:null}
+ },
+
+ mounted(){
+  axios.get('https://script.google.com/macros/s/AKfycbzz-j83MrZomTfLaP-Sb0CsTdFcHGR-qTdPLb4L_GmnSrHrCbM0HUo1qS8G3B6YEiTrUQ/exec?action=getData').then((res) => {this.item=res.data})
+ }
+}
+</script>
+<style>
+  
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+
+    
+    <div>
+       
+      <mcButton/>
+    </div>
+
+
+
+  </template>
+  
+  
+  
+  <script>
+  import mcButton from '../components/mcButton.vue'
+  export default {
+    components: {
+      mcButton
+    }
+  }
+  </script>
+  
+  <style>
+    
+  </style>
